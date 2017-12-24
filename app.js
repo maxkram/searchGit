4b6c8235f5fc0c035cde07a6c8a6b6768ded0916
maxkram <max.kramer@live.com>
2017-12-23 20:09:04 -0600

    finalized with repos

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ searchUser.addEventListener("keyup", (e) => {
 					ui.showAlert("User not found", "alert alert-danger");
 				}else{
 					ui.showProfile(data.profile);
+					ui.showRepos(data.repos);
 				}
 			});
 	}else{
--- github.js
+++ github.js
@@ -2,14 +2,19 @@ class Github {
 	constructor(){
 		this.client_id = "4c22b68748897aed8e6d";
 		this.client_secret = "c34f2c4c26161ce992c2c63d754d35b447846cc2";
+		this.repos_count = 5;
+		this.repos_sort = "created: asc";
 	}
 	async getUser(user){
 		const profileResponse = await fetch(`http://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
-		const profile = await profileResponse.json();
+		const repoResponse = await fetch(`http://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+		const profile = await profileResponse.json();
+		const repos = await repoResponse.json();
 		return {
-			profile
+			profile,
+			repos
 		}
 	}
 }
